ea9dd18d8ca217211c32155ab6fc8dd540bfc046
Daniel Españadero <developer@daniel-espanadero.com>
2022-07-20 00:42:56 +0200

    Dices logic created

---
+++ src/dist/helpers/dices.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rollDices = void 0;
+const rollDices = () => {
+    const dice_1 = Math.floor(6 * Math.random()) + 1;
+    const dice_2 = Math.floor(6 * Math.random()) + 1;
+    const veredict = dice_1 + dice_2 === 7 ? 'win' : 'lose';
+    return {
+        dice_1,
+        dice_2,
+        rollScore: dice_1 + dice_2,
+        veredict
+    };
+};
+exports.rollDices = rollDices;
